bddd82a9707cba283cabf730bc4e73cfec52c5fb
KrystianSzalaj <231944@student.uek.krakow.pl>
2024-05-13 18:17:03 +0200

    24-13-05KS

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,18 @@
 from app import app
-from flask import render_template
+import requests
+from flask import render_template, request, redirect, url_for
 @app.route("/")
 def index():
     return render_template("index.html.jinja")
-@app.route('/extract')
+@app.route('/extract', methods={'POST','GET'})
 def extract():
+    if request.method == 'POST':
+        product_id = request.form.get('product_id')
+        url=f'https://www.ceneo.pl/{product_id}'
+        response = requests.get(url)
+        if response.status_code == requests.codes['ok']:
+            
+            return redirect(url_for('product', product_id=product_id))
     return render_template("extract.html.jinja")
 @app.route('/products')
 def products():
@@ -13,5 +21,5 @@ def products():
 def author():
     return render_template("author.html.jinja")
 @app.route('/product/<product_id>')
-def hello(product_id):
+def product(product_id):
     return render_template("product.html.jinja", product_id=product_id)
