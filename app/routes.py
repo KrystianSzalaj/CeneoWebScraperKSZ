c6dbf26f2b8d5888b11460cbf475819c0adc5f90
KrystianSzalaj <231944@student.uek.krakow.pl>
2024-05-20 18:16:29 +0200

    24-05-20KS

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from app import app
 import requests
+import json
+import os
+from bs4 import BeautifulSoup
 from flask import render_template, request, redirect, url_for
+from app import utils
 @app.route("/")
 def index():
     return render_template("index.html.jinja")
@@ -11,8 +15,29 @@ def extract():
         url=f'https://www.ceneo.pl/{product_id}'
         response = requests.get(url)
         if response.status_code == requests.codes['ok']:
-            
-            return redirect(url_for('product', product_id=product_id))
+            page_dom = BeautifulSoup(response.text,"html.parser")
+            opinions_count = utils.extract(page_dom,"a.product-review__link > span").text.strip()
+            if opinions_count:
+                while(url):
+                    response = requests.get(url)
+                    page_dom = BeautifulSoup(response.text, "html.parser")
+                    opinions = page_dom.select("div.js_product-review")
+                    for opinion in opinions:
+                        single_opinion = {
+                            key : extract(opinion, *value)
+                                for key, value in selectors.items()
+                        }
+                        all_opinions.append(single_opinion)
+                    try:
+                        url = "https://www.ceneo.pl" + extract(page_dom,"a.pagination__next","href")
+                    except TypeError:
+                        url = None
+                    if not os.path.exists("opinions"):
+                        os.mkdir("opinions")
+                    with open(f"opinions/{product_id}")
+                return redirect(url_for('product', product_id=product_id))
+            return render_template("extract.html.jinja", error="dla produktu o podanym kodzie nie ma opinii")
+        return render_template("extract.html.jinja", error="produkt o podanym kodzie nie istnieje")
     return render_template("extract.html.jinja")
 @app.route('/products')
 def products():
