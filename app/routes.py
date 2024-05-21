a0f80f3dec60da50e0e4698e4963499a75a1dc2f
KrystianSzalaj <231944@student.uek.krakow.pl>
2024-05-22 00:06:05 +0200

    24-05-21KS

--- app/routes.py
+++ app/routes.py
@@ -1,50 +1,66 @@
 from app import app
+from app import utils
+import pandas as pd
+import numpy as np
 import requests
 import json
 import os
 from bs4 import BeautifulSoup
+from matplotlib import pyplot as plt
 from flask import render_template, request, redirect, url_for
-from app import utils
+
 @app.route("/")
 def index():
     return render_template("index.html.jinja")
-@app.route('/extract', methods={'POST','GET'})
+
+@app.route("/extract", methods=["POST", "GET"])
 def extract():
-    if request.method == 'POST':
-        product_id = request.form.get('product_id')
-        url=f'https://www.ceneo.pl/{product_id}'
+    if request.method == "POST":
+        product_id = request.form.get("product_id")
+        url = f"https://www.ceneo.pl/{product_id}"
         response = requests.get(url)
-        if response.status_code == requests.codes['ok']:
-            page_dom = BeautifulSoup(response.text,"html.parser")
-            opinions_count = utils.extract(page_dom,"a.product-review__link > span").text.strip()
+        if response.status_code == requests.codes["ok"]:
+            page_dom = BeautifulSoup(response.text, "html.parser")
+            opinions_count = utils.extract(page_dom, "a.product-review__link > span")
             if opinions_count:
-                while(url):
+                url = f"https://www.ceneo.pl/{product_id}#tab=reviews_scroll"
+                all_opinions = []
+                while (url):
                     response = requests.get(url)
                     page_dom = BeautifulSoup(response.text, "html.parser")
                     opinions = page_dom.select("div.js_product-review")
                     for opinion in opinions:
                         single_opinion = {
-                            key : extract(opinion, *value)
-                                for key, value in selectors.items()
+                            key : utils.extract(opinion, *value)
+                                for key, value in utils.selectors.items()
                         }
                         all_opinions.append(single_opinion)
                     try:
-                        url = "https://www.ceneo.pl" + extract(page_dom,"a.pagination__next","href")
+                        url = "https://www.ceneo.pl" + utils.extract(page_dom, "a.pagination__next", "href")
                     except TypeError:
                         url = None
-                    if not os.path.exists("opinions"):
-                        os.mkdir("opinions")
-                    with open(f"opinions/{product_id}")
-                return redirect(url_for('product', product_id=product_id))
-            return render_template("extract.html.jinja", error="dla produktu o podanym kodzie nie ma opinii")
-        return render_template("extract.html.jinja", error="produkt o podanym kodzie nie istnieje")
+                    if not os.path.exists("app/data"):
+                        os.mkdir("app/data")
+                    if not os.path.exists("app/data/opinions"):
+                        os.mkdir("app/data/opinions")
+                    with open(f"app/data/opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
+                        json.dump(all_opinions, jf, indent = 4, ensure_ascii = False)
+                    opinions = pd.DataFrame.from_dict(all_opinions)
+
+                return redirect(url_for('product', product_id = product_id))
+            return render_template("extract.html.jinja", error="Produkt o podanym kodzie nie ma opinii")
+        return render_template("extract.html.jinja", error="Produkt o podanym kodzie nie istnieje")
     return render_template("extract.html.jinja")
-@app.route('/products')
+
+@app.route("/products")
 def products():
+    products = [filename.split(".")[0] for filename in os.listdir("app/data/opinions")]
     return render_template("products.html.jinja")
-@app.route('/author')
+    
+@app.route("/author")
 def author():
     return render_template("author.html.jinja")
-@app.route('/product/<product_id>')
+
+@app.route("/product/<product_id>")
 def product(product_id):
     return render_template("product.html.jinja", product_id=product_id)
